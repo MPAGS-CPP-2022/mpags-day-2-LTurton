b9ab0a6b70c5861ed10fbdf8d2e69ae72babdb9a
L <Logan.Turton@outlook.com>
2022-12-15 10:39:29 +0000

    Beginning to add Cmake builds.

--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -4,6 +4,78 @@
 #include <string>
 #include <vector>
 
+//Declare Functions:
+std::string transformChar(const char in_char);
+
+bool processCommandLine(const std::vector<std::string>& ARGS, 
+                        bool& help_req,bool& ver_req, 
+                        std::string& input_filename,
+                        std::string& output_filename);
+
+int main(int argc, char* argv[])
+{
+    // Command Line Arguments:
+    const std::vector<std::string> INPUT_ARGS{
+        argv, argv + argc};    //Convert inputs to a vector.    
+
+    // Related Variables:
+    bool help_req{false};
+    bool ver_req{false};
+    std::string input_filename{""};
+    std::string output_filename{""};
+
+    const bool cmdLineStatus{processCommandLine(
+        INPUT_ARGS, help_req, ver_req, input_filename, output_filename)};
+
+    // Any failure in argument processing means the function shouldn't run.
+    if (!cmdLineStatus) {
+        return 1;
+    }
+
+    const std::string HELP_STR{
+        "Type a string, then press Ctrl+D to continue after the inputs are transliterated for classical ciphers."};
+    const std::string VER_STR{"The current version is v0.5.0."};
+    //Handle help, requires no further action so return 0; to end program
+    if (help_req) {
+        std::cout
+            << "Usage: mpags-cipher [-h/--help] [--version] [-i <file>] [-o <file>]\n\n"
+            << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
+            << HELP_STR << "Available options:\n\n"
+            << "  -h|--help        Print this help message and exit\n\n"
+            << "  --version        Print version information\n\n"
+            << "  -i FILE          Read text to be processed from FILE\n"
+            << "                   Stdin will be used if not supplied\n\n"
+            << "  -o FILE          Write processed text to FILE\n"
+            << "                   Stdout will be used if not supplied\n\n"
+            << std::endl;
+        return 0;
+    }
+
+    //Handle version, also requires no further action.
+    if (ver_req) {
+        std::cout << VER_STR << "\n";
+        return 0;
+    }
+
+    // Initialise for Cipher:
+    char in_char{};
+    std::string out_str{""};
+
+    // Transliterate input:
+    std::cout << "Please provide some text and press enter, Ctrl + 'd'."
+              << std::endl;
+    while (std::cin >> in_char) {
+        out_str += transformChar(in_char);
+    }
+    // Print the transliterated text:
+    std::cout << "Transliterated text is " << out_str << std::endl;
+    return 0;
+}
+
+/*
+Function Codes:
+*/
+
 std::string transformChar(const char in_char)
 {
     /* transformChar take a character and transliterate to capitalised strings.
@@ -73,18 +145,18 @@ bool processCommandLine(const std::vector<std::string>& ARGS, bool& help_req,
      input_filename - filename from -i article.
      output_filename - filename form -o article.
 
-     return true if succesful, false if error in input pairs.
+     return true if succesful, 1 if error in input pairs.
 
     */
 
     //Define Local Variables
-    const std::size_t N_INPUTS{ARGS.size()};
     bool processStatus{
         true};    // Status flag to indicate whether or not the parsing was successful
 
     // CMD Inputs:
     // skip 0 because this is program name and can be ignored.
-    for (int i{1}; i < N_INPUTS; ++i) {
+    const std::size_t N_INPUTS{ARGS.size()};
+    for (std::size_t i{1}; i < N_INPUTS; ++i) {
         //std::cout << "Argument " << i << " is " << ARGS[i] << std::endl;
         // //Print All Arguements For User.
         if (ARGS[i] == "-h" || ARGS[i] == "--help") {
@@ -128,65 +200,4 @@ bool processCommandLine(const std::vector<std::string>& ARGS, bool& help_req,
         }
     }
     return processStatus;
-}
-
-int main(int argc, char* argv[])
-{
-    // Command Line Arguments:
-    const std::vector<std::string> INPUT_ARGS{
-        argv, argv + argc};    //Convert inputs to a vector.
-    const std::size_t N_INPUTS{INPUT_ARGS.size()};
-
-    // Related Variables:
-    bool help_req{false};
-    bool ver_req{false};
-    std::string input_filename{""};
-    std::string output_filename{""};
-
-    const bool cmdLineStatus{processCommandLine(
-        INPUT_ARGS, help_req, ver_req, input_filename, output_filename)};
-
-    // Any failure in argument processing means the function shouldn't run.
-    if (!cmdLineStatus) {
-        return 1;
-    }
-
-    const std::string HELP_STR{
-        "Type a string, then press Ctrl+D to continue after the inputs are transliterated for classical ciphers."};
-    const std::string VER_STR{"The current version is v0.5.0."};
-    //Handle help, requires no further action so return 0; to end program
-    if (help_req) {
-        std::cout
-            << "Usage: mpags-cipher [-h/--help] [--version] [-i <file>] [-o <file>]\n\n"
-            << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
-            << HELP_STR << "Available options:\n\n"
-            << "  -h|--help        Print this help message and exit\n\n"
-            << "  --version        Print version information\n\n"
-            << "  -i FILE          Read text to be processed from FILE\n"
-            << "                   Stdin will be used if not supplied\n\n"
-            << "  -o FILE          Write processed text to FILE\n"
-            << "                   Stdout will be used if not supplied\n\n"
-            << std::endl;
-        return 0;
-    }
-
-    //Handle version, also requires no further action.
-    if (ver_req) {
-        std::cout << VER_STR << "\n";
-        return 0;
-    }
-
-    // Initialise for Cipher:
-    char in_char{};
-    std::string out_str{""};
-
-    // Transliterate input:
-    std::cout << "Please provide some text and press enter, Ctrl + 'd'."
-              << std::endl;
-    while (std::cin >> in_char) {
-        out_str += transformChar(in_char);
-    }
-    // Print the transliterated text:
-    std::cout << "Transliterated text is " << out_str << std::endl;
-    return 0;
 }
