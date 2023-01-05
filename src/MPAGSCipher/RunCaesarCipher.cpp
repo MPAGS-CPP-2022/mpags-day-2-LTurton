66dafc09eb3311d57c61f580b0c792ae60cc37e1
L <Logan.Turton@outlook.com>
2023-01-05 20:34:14 +0000

    Full use of alphabet namespace.

--- src/MPAGSCipher/RunCaesarCipher.cpp
+++ src/MPAGSCipher/RunCaesarCipher.cpp
@@ -1,4 +1,5 @@
 #include "RunCaesarCipher.hpp"
+#include "Alphabet.hpp"
 
 #include <iostream>
 #include <string>
@@ -7,13 +8,10 @@
 std::string runCaesarCipher(const std::string& inputText, const std::size_t key,
                             const bool encrypt)
 {
-    // Create the alphabet container and output string
-    const std::string ALPHABET{
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    const std::size_t ALPHABET_SIZE{ALPHABET.size()};
+    // Create the a output string    
 
     //Fix The Key in the range 0 - length alphabet
-    const std::size_t filter_key = key % ALPHABET_SIZE;
+    const std::size_t filter_key = key % Alphabet::size;
 
     std::string output_string{};
     int shift_pos{};
@@ -21,28 +19,28 @@ std::string runCaesarCipher(const std::string& inputText, const std::size_t key,
     // For each loop, for each member of array inputText do a thing to orgionalChar:
     for (const auto& origionalChar : inputText) {
         // Find Position in the Alphabet container:
-        for (size_t i{0}; i < ALPHABET_SIZE; ++i) {
-            if (origionalChar == ALPHABET[i]) {
+        for (size_t i{0}; i < Alphabet::size; ++i) {
+            if (origionalChar == Alphabet::alphabet[i]) {
                 // Apply the chosen shift, and determine new character.
                 // Then break out of alphabet loop.
                 if (encrypt) {
-                    shift_pos = (i + filter_key) % ALPHABET_SIZE;
+                    shift_pos = (i + filter_key) % Alphabet::size;
                 } else if (!encrypt) {
                     shift_pos =
-                        (i + ALPHABET_SIZE - filter_key) %
-                        ALPHABET_SIZE;    // +SIZE deals with negative wrapping nicely.
+                        (i + Alphabet::size - filter_key) %
+                        Alphabet::size;    // +SIZE deals with negative wrapping nicely.
                 } else {
                     std::cout
                         << "Encrypt/decrypt not set, defaulting to decryption.";
                     shift_pos =
-                        (i + ALPHABET_SIZE - filter_key) % ALPHABET_SIZE;
+                        (i + Alphabet::size - filter_key) % Alphabet::size;
                 }
                 break;
             }
         }
 
         //Add Ciphered Result to Ouput:
-        output_string += ALPHABET[shift_pos];
+        output_string += Alphabet::alphabet[shift_pos];
     }
 
     // Finally, return the output string
